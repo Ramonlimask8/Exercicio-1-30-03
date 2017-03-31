f33ef8cef4541b4c857ee2e4d373ec69b897032c
Ramon Ferreira <ramonlima0912@gmail.com>
2017-03-30 21:09:53 -0300

    dobro de um numero

---
+++ src/exercicio1dobrodonumero/Exercicio1DobrodoNumero.java
@@ -0,0 +1,27 @@
+
+package exercicio1dobrodonumero;
+
+import java.util.Scanner;
+
+public class Exercicio1DobrodoNumero {
+
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner (System.in);
+        
+        System.out.print("N1: ");
+        int n = leitor.nextInt();
+          int r = 2 * n;
+        
+        if (n < 1 || n > 12){
+             System.out.print("Entre com0 um numero entre 1 e 12...  ");
+        System.out.print(r);
+    }else {
+       
+         
+            
+    }
+        
+    }
+    
+}
